754e730ba51c08787c70643ebcacf363dd09b525
alja <amraktadel@ucsd.edu>
2023-10-10 13:13:41 -0700

    Implement only-if-cached cache control using XrdPfcFsctl.

--- src/XrdHttp/XrdHttpReq.cc
+++ src/XrdHttp/XrdHttpReq.cc
@@ -865,6 +865,8 @@ void XrdHttpReq::mapXrdErrorToHttpStatus() {
       case kXR_InvalidRequest:
         httpStatusCode = 405; httpStatusText = "Method is not allowed";
         break;
+      case kXR_ReqTimedOut:
+        httpStatusCode = 504; httpStatusText = "Gateway timeout";
       default:
         break;
     }
